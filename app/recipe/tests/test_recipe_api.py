f344ae6fc2acf29f18b6892b9a936ebec34efc8e
abhay734 <vermaabhay734@gmail.com>
2024-06-24 18:20:38 +0530

    Fixed Line Spacing for Lint & Test.

--- app/recipe/tests/test_recipe_api.py
+++ app/recipe/tests/test_recipe_api.py
@@ -18,13 +18,7 @@ from core.models import Recipe
 
 from recipe.serializers import RecipeSerializer
 
-
-
-
 RECIPES_URL = reverse('recipe:recipe-list')
-
-
-
 
 def create_recipe(user, **params):
     """Create and return a sample recipe."""
@@ -37,34 +31,25 @@ def create_recipe(user, **params):
     }
     defaults.update(params)
 
-
     recipe = Recipe.objects.create(user=user, **defaults)
     return recipe
-
-
 
 
 class PublicRecipeAPITests(TestCase):
     """Test unauthenticated API requests."""
 
-
     def setUp(self):
         self.client = APIClient()
-
 
     def test_auth_required(self):
         """Test auth is required to call API."""
         res = self.client.get(RECIPES_URL)
 
-
         self.assertEqual(res.status_code, status.HTTP_401_UNAUTHORIZED)
-
-
 
 
 class PrivateRecipeApiTests(TestCase):
     """Test authenticated API requests."""
-
 
     def setUp(self):
         self.client = APIClient()
@@ -74,21 +59,17 @@ class PrivateRecipeApiTests(TestCase):
         )
         self.client.force_authenticate(self.user)
 
-
     def test_retrieve_recipes(self):
         """Test retrieving a list of recipes."""
         create_recipe(user=self.user)
         create_recipe(user=self.user)
 
-
         res = self.client.get(RECIPES_URL)
-
 
         recipes = Recipe.objects.all().order_by('-id')
         serializer = RecipeSerializer(recipes, many=True)
         self.assertEqual(res.status_code, status.HTTP_200_OK)
         self.assertEqual(res.data, serializer.data)
-
 
     def test_recipe_list_limited_to_user(self):
         """Test list of recipes is limited to authenticated user."""
@@ -99,9 +80,7 @@ class PrivateRecipeApiTests(TestCase):
         create_recipe(user=other_user)
         create_recipe(user=self.user)
 
-
         res = self.client.get(RECIPES_URL)
-
 
         recipes = Recipe.objects.filter(user=self.user)
         serializer = RecipeSerializer(recipes, many=True)
